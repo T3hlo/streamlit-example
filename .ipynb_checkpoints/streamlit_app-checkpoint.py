070f0c4e1b89ef6cda260f00d2623da63b141d23
Thilo Reich <45871693+T3hlo@users.noreply.github.com>
2020-12-09 08:27:18 +0000

    update

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -17,6 +17,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
+
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
@@ -46,6 +48,30 @@ with st.echo(code_location='below'):
 )
         
         )
+
+    
+
+
+
+with st.echo(code_location='below'):
+
+
+    # Compute x^2 + y^2 across a 2D grid
+    x, y = np.meshgrid(range(-5, 5), range(-5, 5))
+    z = x ** 2 + y ** 2
+
+    # Convert this grid to columnar data expected by Altair
+    source = pd.DataFrame({'x': x.ravel(),
+                         'y': y.ravel(),
+                         'z': z.ravel()})
+
+    st.altair_chart(alt.Chart(source).mark_rect().encode(
+    x='x:O',
+    y='y:O',
+    color='z:Q'
+)              
+        
+  )
 
     
 
